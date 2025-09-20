d130bade2a980e719c82487e9e44470cf8f25794
suri-xoxo <shsuri15@gmail.com>
2025-09-19 21:15:13 -0700

    Update area_2d.gd

--- area_2d.gd
+++ area_2d.gd
@@ -5,16 +5,12 @@ extends Area2D
 @onready var camera_2d: Camera2D = %Camera2D
 
 func _on_body_entered(body: Node2D) -> void:
-
 	if body.name == "Frog":
-		var tween: Tween = create_tween()
-
 		if canvas_modulate:
-			tween.tween_property(canvas_modulate, "color:a", 0.0, 0.5)
+			canvas_modulate.color.a = 0.0
 		if point_light_2d:
-			tween.tween_property(point_light_2d, "energy", 0.0, 0.5)
+			point_light_2d.energy = 0.0
 		if camera_2d:
-			tween.tween_property(camera_2d, "zoom", Vector2(1.5, 1.5), 0.5)
-			
-		await tween.finished
+			camera_2d.zoom = Vector2(1.5, 1.5)
+		
 		queue_free()
